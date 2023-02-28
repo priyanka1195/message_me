e6e299a150d4e8346c3ca59158f4575dd7d0c35b
Priyanka <goyalpriyanka1995@gmail.com>
2023-02-28 15:15:37 +0530

    build messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,9 @@ User.create(username: "Anne", password: "password")
 User.create(username: "Pier", password: "password")
 User.create(username: "Mint", password: "password")
 
+Message.create(body: "Hi!", user: User.second)
+Message.create(body: "Hello John", user: User.third)
+Message.create(body: "How are you Johnes", user: User.first)
+Message.create(body: "Hi! all", user: User.fifth)
+Message.create(body: "I am all good", user: User.third)
+
